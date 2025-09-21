a1df143545c52bcdc1da06ebc778e001ba2668b2
Gowtham E <gowe@cisco.com>
2025-09-21 12:02:37 +0530

    works well with groups and alias also:

--- bot_main.py
+++ bot_main.py
@@ -1,7 +1,10 @@
 from webex_bot.webex_bot import WebexBot
-from handlers.status_command import JenkinsStatusCommand
+from handlers.status_command import JenkinsStatusCommand, USMStatusCommand, IMSStatusCommand, StatusCommand
 from config import WEBEX_BOT_TOKEN
 
 bot = WebexBot(WEBEX_BOT_TOKEN)
 bot.add_command(JenkinsStatusCommand())
+bot.add_command(USMStatusCommand())
+bot.add_command(IMSStatusCommand())
+bot.add_command(StatusCommand())
 bot.run()
